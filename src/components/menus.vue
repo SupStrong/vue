1b81faaa259b770698333da77b0bba13fb6eada6
liuchen <1458380717@qq.com>
2020-11-04 18:14:22 +0800

    优化布局

--- src/components/menus.vue
+++ src/components/menus.vue
@@ -14,7 +14,6 @@
           <el-menu-item index="1-2">选项2</el-menu-item>
           <el-menu-item index="1-1">选项1</el-menu-item>
           <el-menu-item index="1-2">选项2</el-menu-item>
-          
           <el-menu-item index="1-1">选项1</el-menu-item>
           <el-menu-item index="1-2">选项2</el-menu-item>
           <el-menu-item index="1-1">选项1</el-menu-item>  
