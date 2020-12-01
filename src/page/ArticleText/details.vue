751c28aef92a7b6db34817bc863339db0b808bb2
SupStrong <1458380717@qq.com>
2020-12-01 20:59:28 +0800

    commit

--- src/page/ArticleText/details.vue
+++ src/page/ArticleText/details.vue
@@ -26,6 +26,20 @@
             <el-time-picker placeholder="选择时间" v-model="form.date2"></el-time-picker>
           </el-col>
         </el-form-item>
+        <el-form-item label="视频上传" prop="Video">
+          <!-- action必选参数, 上传的地址 -->
+            <el-upload class="avatar-uploader el-upload--text" action="/aa"  list-type="picture-card" :show-file-list="false" :on-success="handleVideoSuccess" :before-upload="beforeUploadVideo" :on-progress="uploadVideoProcess">
+                <video v-if="videoForm.Video !='' && videoFlag == false" :src="videoForm.Video" class="avatar" controls="controls">您的浏览器不支持视频播放</video>
+                <i class="el-icon-plus"></i>
+            </el-upload>
+            <P class="text">请保证视频格式正确，且不超过10M</P>
+        </el-form-item>
+        <el-form-item label="营销类型">
+         <el-radio-group v-model="radio">
+          <el-radio :label="1">文章</el-radio>
+          <el-radio :label="2">视频</el-radio>
+        </el-radio-group>
+        </el-form-item>
         <el-form-item label="状态">
           <el-switch v-model="form.status"></el-switch>
         </el-form-item>
@@ -58,6 +72,12 @@ import editors from '../../components/edit.vue';
   export default {
     data() {
       return {
+        videoUploadPercent:"",
+        videoFlag:true,
+        videoForm:{
+          videoUploadId:"",
+          video:""
+        },
         form: {
           title: '',
           create_date: '',
@@ -80,7 +100,33 @@ import editors from '../../components/edit.vue';
       },
       onSubmit() {
         console.log(this.form.textarea,'submit!');
-      }
+      },
+      beforeUploadVideo(file) {
+        const isLt10M = file.size / 1024 / 1024  < 10;
+        if (['video/mp4', 'video/ogg', 'video/flv','video/avi','video/wmv','video/rmvb'].indexOf(file.type) == -1) {
+            this.$message.error('请上传正确的视频格式');
+            return false;
+        }
+        if (!isLt10M) {
+            this.$message.error('上传视频大小不能超过10MB哦!');
+            return false;
+        }
+      },
+      uploadVideoProcess(event, file, fileList){
+          this.videoFlag = true; 
+          this.videoUploadPercent = file.percentage.toFixed(0);
+      },
+      handleVideoSuccess(res, file) {
+            //获取上传图片地址
+          this.videoFlag = false;
+          this.videoUploadPercent = 0;
+          if(res.status == 200){
+              this.videoForm.videoUploadId = res.data.uploadId;
+              this.videoForm.Video = res.data.uploadUrl;
+          }else{
+              this.$message.error('视频上传失败，请重新上传！');
+          }
+      },
     },
     components:{
       editors
