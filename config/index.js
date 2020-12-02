5f7cb1a3a3e91c942c65a75a420dca2331c67f3b
SupStrong <1458380717@qq.com>
2020-12-02 22:30:31 +0800

    修改上传图片

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ module.exports = {
             target: 'http://localhost:7001',//后端接口地址
             changeOrigin: true,//是否允许跨越
             pathRewrite: {
-                '^/api': '/api',//重写,
+              '^/api/': '/' 
             }
         }
     },
